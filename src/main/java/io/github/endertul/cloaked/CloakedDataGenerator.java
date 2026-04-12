c188f1954d5ef30a73c39368993fe4b4d2f8d868
Endertul0 <shawn.shireman.5@gmail.com>
2026-04-11 20:32:30 -0700

    Before refactor

---
+++ src/main/java/io/github/endertul/cloaked/CloakedDataGenerator.java
@@ -0,0 +1,11 @@
+package io.github.endertul.cloaked;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class CloakedDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
