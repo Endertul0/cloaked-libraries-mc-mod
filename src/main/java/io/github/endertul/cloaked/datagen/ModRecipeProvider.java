fb80c56f4c054eb8d8bb3781846c4489b472f756
Endertul0 <shawn.shireman.5@gmail.com>
2026-04-12 18:15:22 -0700

    1.20.2 update (no entity support yet)

--- src/main/java/io/github/endertul/cloaked/datagen/ModRecipeProvider.java
+++ src/main/java/io/github/endertul/cloaked/datagen/ModRecipeProvider.java
@@ -3,6 +3,7 @@ package io.github.endertul.cloaked.datagen;
 import io.github.endertul.cloaked.item.ModItems;
 import net.fabricmc.fabric.api.datagen.v1.FabricDataOutput;
 import net.fabricmc.fabric.api.datagen.v1.provider.FabricRecipeProvider;
+import net.minecraft.data.server.recipe.RecipeExporter;
 import net.minecraft.data.server.recipe.RecipeJsonProvider;
 import net.minecraft.data.server.recipe.ShapedRecipeJsonBuilder;
 import net.minecraft.item.Items;
@@ -20,7 +21,7 @@ public class ModRecipeProvider extends FabricRecipeProvider {
 	}
 	
 	@Override
-	public void generate(Consumer<RecipeJsonProvider> exporter) {
+	public void generate(RecipeExporter exporter) {
 		//offerSmelting(exporter, RUBY_SMELTABLES, RecipeCategory.MISC, ModItems.RUBY,
 		//        0.7f, 200, "ruby");
 		//offerBlasting(exporter, RUBY_SMELTABLES, RecipeCategory.MISC, ModItems.RUBY,
